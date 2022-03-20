bc9e66a7a7b8ee004f2da814ddb83caafddc83de
WSLimmm <caker97@nate.com>
2022-03-20 11:50:18 +0900

    Let's be CEO

--- client/src/App.js
+++ client/src/App.js
@@ -20,13 +20,12 @@ import { locations } from './components/Map/Locates';
 import Infomation from './components/Map/Infomation';
 import { useState } from 'react';
 
-// fuck
 function App() {
   const [place, setPlace] = useState("기본")
   
   return (
     <BrowserRouter>
-      <Container fluid>
+      <Container fluid>  
         <Row>
           <Col sm={10}>
             <Header></Header>
